6e720c7fdad52eddb01231cd6928acae5aa4bd05
Andy Kim <andy.chiwon.kim@gmail.com>
2025-01-08 19:37:52 +0900

    0.1.14

--- ppt2video/tools.py
+++ ppt2video/tools.py
@@ -77,7 +77,7 @@ def _clean_text(input_text):
 
     # Remove non-Korean, non-English chars, non-numbers, and special characters 
     # except commas, periods, question marks, exclamation marks, spaces, %, $, &, -
-    input_text = re.sub(r'[^a-zA-Z0-9가-힣.,?!%\$\n\s&-]', '', input_text)
+    input_text = re.sub(r'[^a-zA-Z0-9가-힣.,?!%\$\n\s&-/]', '', input_text)
 
     # Remove all newlines
     input_text = input_text.replace('\n', '') 
